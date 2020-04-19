26d34793f096e2d8466d428d1a9b7268369e3d89
code-ninza <i_think_sanu@hotmail.com>
2020-04-19 08:52:27 +0530

    Some security changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import "package:flutter/material.dart";
 import "package:google_fonts/google_fonts.dart";
 import "package:covid_india_tracker/screens/india_stats.dart";
+import "package:syncfusion_flutter_core/core.dart";
 
-void main() => runApp(HomePage());
+void main() {
+  SyncfusionLicense.registerLicense(
+      "NT8mJyc2IWhia31ifWN9Z2FoYmF8YGJ8ampqanNiYmlmamlmanMDHmg6DCc7Oj04DCAyPSYTOzwnPjI6P30wPD4=");
+  return runApp(HomePage());
+}
 
 class HomePage extends StatefulWidget {
   @override
